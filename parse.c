3b2f4c9c388abf49786d2fec65ff4393d97a9569
Tsukuda Yoshio <tsukuda.yoshio@gmail.com>
2020-06-25 14:46:08 +0900

    値を取らないリターンを追加

--- parse.c
+++ parse.c
@@ -1122,7 +1122,7 @@ static Node *stmt(void) {
   return node;
 }
 
-// stmt2 = "return" expr ";"
+// stmt2 = "return" expr? ";"
 //       | "if" "(" expr ")" stmt ("else" stmt)?
 //       | "switch" "(" expr ")" stmt
 //       | "case" const-expr ":" stmt
@@ -1140,6 +1140,9 @@ static Node *stmt(void) {
 static Node *stmt2(void) {
   Token *tok;
   if ((tok = consume("return"))) {
+    if (consume(";"))
+      return new_node(ND_RETURN, tok);
+
     Node *node = new_unary(ND_RETURN, expr(), tok);
     expect(";");
     return node;
